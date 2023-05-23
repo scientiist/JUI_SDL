045b7afc98c1f08ad874348c53f27682ea624365
scientiist <j0sh.oleary11@gmail.com>
2023-05-23 17:44:52 -0500

    Demo game sorce

--- src/TestBench/JUIDemoGame.cpp
+++ src/TestBench/JUIDemoGame.cpp
@@ -7,25 +7,38 @@ void JUIDemoGame::Initialize() {
 
     JUI::Rect *rect = new JUI::Rect();
     rect->SetParent(Gui);
+    rect->SetName("YA NOTHIN");
     rect->SetBackgroundColor({255, 0, 0, 0});
     rect->SetBorderColor({255, 255, 255, 0});
-    rect->SetSize(JUI::UDim2{0,0, 0.5f, 0.5f});
+    rect->SetSize({0,0, 1, 1});
+    rect->SetPosition({0, 0, 0.1, 0.3});
 
     JUI::Rect *rect2 = new JUI::Rect();
     rect2->SetParent(rect);
+    rect2->SetName("NO TALENT");
     rect2->SetBackgroundColor({0,0,255,0});
-    rect2->SetSize(JUI::UDim2{0, 0, 1.0f, 0.5f});
+    rect2->SetSize({0, 0, 1.0f, 0.5f});
+    rect2->SetPosition({10, 10, 0, 0});
 
     // TODO: Implement Default Font
 
-    JUI::Text *text = new JUI::Text();
-    text->SetParent(rect2);
-    text->Content = "42069";
+    Font nigger = Font(renderer, "fonts/FreeSans.ttf", 18);
 
+    JUI::Text *text = new JUI::Text();
+    text->SetParent(rect);
+    text->SetName("GO HOME AND PRACTICE");
+    text->SetFont(nigger);
+    text->SetTextColor({255, 255, 255, 255});
+    text->SetText("HELLO WORLD");
 }
 
 void JUIDemoGame::Update(float delta) {
     SDLGame::Update(delta);
+
+    for (auto& child : Gui->GetChildren())
+    {
+        //std::cout << child->GetName() << std::endl;
+    }
 
 }
 
